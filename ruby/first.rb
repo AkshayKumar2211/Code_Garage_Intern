26ca428fd5c57772f5f3d54811b4119211b650d0
AkshayKumar2211 <taara0361@gmail.com>
2026-03-02 15:51:34 +0530

    first ruby program

---
+++ ruby/first.rb
@@ -0,0 +1,14 @@
+# To Print hello worlds use puts
+puts "Hello World::";
+
+puts "First program in ruby";
+
+
+#how to declare function in ruby....
+def firstFunction()
+        puts"This is my first function...";
+end
+
+
+firstFunction();
+  
